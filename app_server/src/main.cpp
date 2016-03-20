b7879812cf256acce6857d13eb3102614abd3d7c
Diego <diegofk26@gmail.com>
2016-03-20 16:49:06 -0300

    Se realizo la integracion de la libreria rocksdb con un pequenio main de prueba

--- app_server/src/main.cpp
+++ app_server/src/main.cpp
@@ -1,8 +1,36 @@
-#include "WebServer.h"
+#include <cassert>
+#include "rocksdb/db.h"
 #include <iostream>
 
-int main() {
-	WebServer ws;
-	std::cout << "Hola Mundo \n";
+
+
+
+int main(){
+	
+	rocksdb::DB* db;
+	rocksdb::Options options;
+	options.create_if_missing = true;
+	
+	rocksdb::Status status =
+	  rocksdb::DB::Open(options, "./testdb", &db);
+	  
+	if (!status.ok()) std::cerr << status.ToString() << std::endl;
+	assert(status.ok());
+	
+	std::string value = "Hola";
+	std::string key1 = "clave1";
+	std::string key2 = "clave2";
+	
+	rocksdb::Status s;
+	
+	
+	s = db->Put(rocksdb::WriteOptions(), key2, value);
+	
+	std::string val;
+	
+	s = db->Get(rocksdb::ReadOptions(), key2, &val);
+
+
+	std::cout << val << std::endl;
 	return 0;
 }
