c257df66e51fd5c95a14e0d18b947cd8bb435fbb
seguijoaquin <segui.joaquin@gmail.com>
2016-03-25 17:17:00 -0300

    Update instalar_rocksdb.sh

--- instalar_rocksdb.sh
+++ instalar_rocksdb.sh
@@ -9,12 +9,12 @@ echo -e '\e[0;32mDEPENDENCIAS INSTALADAS\e[0m'
 echo '--------------------------'
 echo -e '\e[0;31mInstalando libreria RocksDB\e[0m'
 cd app_server/lib/rocksdb-master/
-echo -e '\e[0;31mcd app_server/lib/rocksdb-master/\e[0m'
+echo -e '\e[0;34mcd app_server/lib/rocksdb-master/\e[0m'
 make static_lib
-echo -e '\e[0;31mmake static_lib\e[0m'
+echo -e '\e[0;34mmake static_lib\e[0m'
 mv -i librocksdb.a ../
-echo -e '\e[0;31mmv -i librocksdb.a ../\e[0m'
+echo -e '\e[0;34mmv -i librocksdb.a ../\e[0m'
 cp -r include/* ../
-echo -e '\e[0;31mcp -r include/* ../\e[0m'
+echo -e '\e[0;34mcp -r include/* ../\e[0m'
 echo -e '\e[0;32mROCKSDB INSTALADA\e[0m'
 cd ../../../
