4d12a207e2d7a3228b924f946e2383cbf0944b5d
juanmafc <juanmafc25@gmail.com>
2016-04-26 10:59:05 -0300

    Agrego pruebas de la API y la base de datos que se necesita para que corran bien. Issue #17

--- ApiUnitTesting/AppServerApiUnitTests.py
+++ ApiUnitTesting/AppServerApiUnitTests.py
@@ -6,13 +6,57 @@ import unittest
 
 # Precondiciones:
 # - La base de datos del servidor no debe contener usuarios.
-# - El servidor debe cumplir las especificaciones de la api.
+# - El servidor debe cumplir las especificaciones de la api.#
+# - La base de datos tiene un usuario registrado con el nombre "usuario" y password "password"
+# Login:
+# - La base de datos NO tiene un usuario registrado con el nombre "usuarioFalso" y password "passwordFalso"
+# Registro:
+# - La base de datos NO tiene un usuario registrado con el nombre "usuarioNuevo" y password "passwordNuevo"
+
+
+Address = "http://localhost:8000"
+#Tal vez mandar las URIs a sus respectivas clases
+URILogin = "/login"
+URIResgistro = "/registro"
+
+usuarioCorrecto = 'usuario'
+passwordCorrecto = 'password'
+
+def crearHeadersDeUsuarioYPassword(usuario, password):
+    return {'Usuario': usuario,'Password': password }
 
 class TestLogin(unittest.TestCase):
 
-    port = 8000
+    msgLoginCorrecto = "Se logueo correctamente"
+    msgLoginIncorrecto = "No coinciden los datos"
 
-    URI = 'http://10.1.151.198:' + str(port);
+    usuarioIncorrecto = 'usuarioFalso'    
+    passwordIncorrecto = 'passwordFalso'    
+
+    def test_LoginConUnUsuarioRegistrado(self):        
+        headUsuarioRegistrado = crearHeadersDeUsuarioYPassword( usuarioCorrecto, passwordCorrecto)
+        request = requests.get(Address + URILogin,headers=headUsuarioRegistrado)
+        self.assertEqual(request.status_code,200)
+        self.assertEqual(request.reason,self.msgLoginCorrecto)
+        self.assertIsNotNone(request.headers["Token"])
+
+    def test_LoginConUnUsuarioYPasswordIncorrectos(self):
+        headUsuarioYPasswordIncorrectos = crearHeadersDeUsuarioYPassword( self.usuarioIncorrecto, self.passwordIncorrecto)
+        request = requests.get(Address + URILogin,headers=headUsuarioYPasswordIncorrectos)
+        self.assertEqual(request.status_code,400)
+        self.assertEqual(request.reason,self.msgLoginIncorrecto)
+
+    def test_LoginConUnUsuarioIncorrecto(self):        
+        headUsuarioIncorrecto = crearHeadersDeUsuarioYPassword( self.usuarioIncorrecto, passwordCorrecto)
+        request = requests.get(Address + URILogin,headers=headUsuarioIncorrecto)
+        self.assertEqual(request.status_code,400)
+        self.assertEqual(request.reason,self.msgLoginIncorrecto)
+
+    def test_LoginConUnPasswordIncorrecto(self):        
+        headPasswordIncorrecto = crearHeadersDeUsuarioYPassword( usuarioCorrecto, self.passwordIncorrecto)
+        request = requests.get(Address + URILogin,headers=headPasswordIncorrecto)
+        self.assertEqual(request.status_code,400)
+        self.assertEqual(request.reason,self.msgLoginIncorrecto)
 
     # --------------------------------------------------------------------------
     # ----------------------------- Status CODE --------------------------------
@@ -20,14 +64,6 @@ class TestLogin(unittest.TestCase):
 
 
     #   Tests que deberian dar status code 400 <--------------------------------
-
-    # def test_LoginConUnUsuarioInvalido(self):
-    #     head = {'Usuario': 'Frezzer',
-    #     'PassWord': '123456'}
-    #     request = requests.get(self.URI +'/login',headers=head)
-    #     self.assertEqual(request.status_code,400)
-
-
 
     #VER QUE ONDA SI ES RESPONSABILIDAD DEL APP SERVER O DEL CLIENTE.
 
@@ -82,21 +118,23 @@ class TestLogin(unittest.TestCase):
 
 class TestRegistro(unittest.TestCase):
 
-    port = 8000
+    headUsuarioYaRegistrado = crearHeadersDeUsuarioYPassword(usuarioCorrecto, 'cualquierPassword')
+    msgUsuarioYaRegistrado = "Usuario existente"
+    msgUsuarioNuevoRegistrado = "Se pudo registrar el usuario"
 
-    URI = 'http://10.1.151.198:' + str(port);
+    usuarioNuevo = "usuarioNuevo"
+    passwordNuevo = "passwordNuevo"
+    headUsuarioNuevo = crearHeadersDeUsuarioYPassword(usuarioNuevo, passwordNuevo)
 
+    def test_RegistroDeUnUsuarioYaRegistrado(self):
+        request = requests.put(Address + URIResgistro, headers=self.headUsuarioYaRegistrado)
+        self.assertEqual(request.status_code,400)
+        self.assertEqual(request.reason,self.msgUsuarioYaRegistrado)
 
-    def test_RegistroDeUnUsuarioValido(self):
-        head = {
-            'Usuario': 'Usuariooooasdasooooo',
-            'PassWord': '123456'
-        }
-
-
-        request = requests.put(self.URI+'/registro',headers=head)
-        self.assertEqual(request.status_code,200)
-
+    def test_RegistroDeUnUsuarioNuevo(self):
+        request = requests.put(Address + URIResgistro, headers=self.headUsuarioNuevo)
+        self.assertEqual(request.status_code,201)
+        self.assertEqual(request.reason,self.msgUsuarioNuevoRegistrado)
 
 
 
