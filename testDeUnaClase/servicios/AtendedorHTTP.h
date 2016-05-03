591b7af6c52464922370f4c17b463f56b1191e42
diegofk26 <diegofk26@gmail.com>
2016-05-03 00:53:33 -0300

    Subo el test de una clase con mongoose incluido

---
+++ testDeUnaClase/servicios/AtendedorHTTP.h
@@ -0,0 +1,56 @@
+#ifndef ATENDEDORHTTP_H
+#define ATENDEDORHTTP_H
+
+#include "StringUtil.h"
+#include <map>
+#include "MensajeHTTPRequest.h"
+
+
+
+#include "ConstantesEventosServicios.h"
+
+
+class AtendedorHTTP
+{
+    public:
+        //AtendedorHTTP(http_message* mensajeHTTP, map<string,string>* tokensDeUsuarios);
+        AtendedorHTTP(MensajeHTTPRequest mensajeHTTP, map<string,string>* tokensDeUsuarios);
+        virtual ~AtendedorHTTP();
+
+        ServicioALanzar getServicioALanzar();
+    protected:
+    private:
+        /*Por ahora solo la necesito para llamar al registrador y al logginer, tal vez quede mejor poniendolo como
+         una variable estatica de una clase "Servicio"
+         Lo mismo con el manager....
+         La otra serie hacer que esta clase setee EVENTOS en el Server y que otra clase dentro del server se encargue de instanciar los servicios
+         Lo malo de ese caso es que si quiero agregar un servicio tendria que agreagarlo al atendedor, a la lista de eventos y al case que maneje lanzar los servicios
+         Lo bueno es que AtendedorHTTP solo se manejaria con los parametros que necesita
+
+         Lo malo de ESTA implementacion (la que estoy usando) es que si necesito lanzar un servicio con otro tipo de parametro, tendria que agregarlo al constructor
+         y a la larga podria quedar un constructor enorme
+         Lo bueno es que si quiero agregar un servicio solo tengo que hacerlo en una porcion especifica de codigo, en vez de 3
+
+         Creo que lo que define es la cantidad de cosas diferentes que necesite (y si se las tendría que pasar al atendedorHTTP)
+         por ejemplo conversaciones
+         O podria poner esas cosas como variables de clase(staticas)
+
+         EDIT: termine haciendo el disenio con EVENTOS porque era el que menos NO me convencia
+
+        */
+        ServicioALanzar servicioALanzar;
+
+        //Por ahora dejo el http_message porque solo quiero probar MensajeHTTPRequest aca
+        //http_message* mensajeHTTP;
+        MensajeHTTPRequest mensajeHTTPRequest;
+
+
+        map<string,string>* tokensDeUsuarios;
+
+        void atenderMesajeHTTP();
+        bool tienePermiso();
+        bool compararMetodoHTTP( string metodo);
+        bool compararUriHTTP(string uri);
+};
+
+#endif // ATENDEDORHTTP_H
---
+++ testDeUnaClase/servicios/servicioLogin.cpp
@@ -0,0 +1,92 @@
+#include "servicioLogin.h"
+
+/*servicioLogin::servicioLogin(http_message* mensajeHTTP, rocksdb::DB* dbUsuarios,  map<string,string>* tokensDeUsuarios ){
+    this->mensajeHTTP = MensajeHTTPRequest(mensajeHTTP,0);
+    this->dbUsuarios = dbUsuarios;
+    this->tokensDeUsuarios = tokensDeUsuarios;
+    this->atenderLogin();
+}*/
+
+servicioLogin::servicioLogin(MensajeHTTPRequest mensajeHTTP, rocksdb::DB* dbUsuarios,  map<string,string>* tokensDeUsuarios ){
+    this->mensajeHTTP = mensajeHTTP;
+    this->dbUsuarios = dbUsuarios;
+    this->tokensDeUsuarios = tokensDeUsuarios;
+    this->atenderLogin();
+}
+
+
+void servicioLogin::atenderLogin(){
+    if (usuarioExiste()){
+        realizarLogin();
+    }
+    else{
+        //SI ALGUNO DE LOS DOS DATOS ESTA INCORRECTO, SE LE MANDA UN MENSAJE GENERAL DE QUE HAY DATOS INCORRECTOS
+        //Refactorizar: respuesta = MENSAJE_DATOS__LOGIN_ERRONEOS;
+        this->respuesta = "HTTP/1.1 400 No coinciden los datos\r\n\r\n";
+    }
+}
+
+bool servicioLogin::usuarioExiste(){
+    //Tengo que sacar las credenciales
+    //Tengo que ver si son correctas
+    //Devuelve T o F
+    //metodo getUsuarioYPassword(.....)
+    /*mg_str* headerUsuario = mg_get_http_header(mensajeHTTP, "Usuario");
+    mg_str* headerPassword = mg_get_http_header(mensajeHTTP, "Password");
+
+    string usuarioIngresado(headerUsuario->p,headerUsuario->len);
+    string passwordIngresado(headerPassword->p,headerPassword->len);
+    */
+
+    string usuarioIngresado = this->mensajeHTTP.getHeader("Usuario");
+    string passwordIngresado = this->mensajeHTTP.getHeader("Password");
+
+    string passwordGuardado;
+    rocksdb::Status estado = this->dbUsuarios->Get(rocksdb::ReadOptions(), usuarioIngresado, &passwordGuardado );
+
+    if (estado.IsNotFound()){
+        return false;
+    }
+    else{
+        if(passwordGuardado == passwordIngresado){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+
+
+void servicioLogin::realizarLogin(){
+    string token;
+    token = generarToken();
+    //Refactorizar: TODO guardar el token en algun lugar token-usuario
+    //Refactorizar: esto esta copiado de arriba, ¿archivo Utilities"?
+
+    /*mg_str* headerUsuario = mg_get_http_header(mensajeHTTP, "Usuario");
+    string usuarioIngresado(headerUsuario->p,headerUsuario->len);
+    */
+    string usuarioIngresado = this->mensajeHTTP.getHeader("Usuario");
+
+    (*(this->tokensDeUsuarios))[usuarioIngresado] = token;
+
+    this->respuesta = "HTTP/1.1 200 Se logueo correctamente\r\nToken:" + token + "\r\n\r\nbody\0";
+}
+
+string servicioLogin::generarToken(){
+    //Algo random
+    return "123456abcdef";
+
+}
+
+
+
+string servicioLogin::getRespuesta(){
+    return this->respuesta;
+}
+
+servicioLogin::~servicioLogin(){
+    //dtor
+}
