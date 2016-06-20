7f8de342d2a056d6c186efc9a900c8d343b624c1
juanmafc <juanmafc25@gmail.com>
2016-06-20 17:30:13 -0300

    #50 Agrego una prueba para que se encuentren candidatos

--- Appserver/Test/ApiUnitTesting/testBusquedaCandidatos.py
+++ Appserver/Test/ApiUnitTesting/testBusquedaCandidatos.py
@@ -43,8 +43,10 @@ class TestBusquedaCandidatos(unittest.TestCase):
     #Una categoria que SI o SI esta en el Shared
     categoriaValida  = "outdoors"
     interesUnico = "INTERES UNICO QUE NO TIENE NADIE MAS"
+    interesCompartido = "INTERES QUE SOLO DEBE SER COMPARTIDO POR DOS USUARIOS"
 
     msgNoSeEncontraronCandidatos = "Candidato no encontrado"
+    msgSeEncontraronCandidatos = "Candidato encontrado"
 
 
     def agregarEmailAlUsuario(self, bodyUsuario, email):
@@ -75,10 +77,11 @@ class TestBusquedaCandidatos(unittest.TestCase):
         #Para esto no debe haber ningun usuario en el shared con el interes "interesUnico"
         #Aca creo el body del usuario con un interes unico, ningun otro lo debe usar
         nombreUsuario = "test_UsuarioPideUnCandidatoPeroNoSeEncuentra.com"
+
         bodyUsuario = abrirJson(self.usuarioSinIntereses)
         self.agregarEmailAlUsuario(bodyUsuario, nombreUsuario)
         self.agregarValorDeInteresAlUsuario(bodyUsuario, self.interesUnico)
-        headRegistrarUsuario = crearHeadersParaRegistro("test_mandoUnRegistroAlServer")
+        headRegistrarUsuario = crearHeadersParaRegistro(nombreUsuario)
         replyRegistro = requests.put(Address + URIResgistro, headers=headRegistrarUsuario, data=json.dumps(bodyUsuario))
 
         #Se loguea
@@ -93,3 +96,48 @@ class TestBusquedaCandidatos(unittest.TestCase):
         self.assertEqual(replyPedirCandidatos.status_code,201)
 
         self.usuariosParaBorrar.extend([nombreUsuario])
+
+    def crearBodyConUnInteres(self, email, interes):
+        bodyUsuario = abrirJson(self.usuarioSinIntereses)
+        self.agregarEmailAlUsuario(bodyUsuario, email)
+        self.agregarValorDeInteresAlUsuario(bodyUsuario, interes)
+        return bodyUsuario
+
+    def registrarUsuario(self, nombreUsuario, bodyUsuario):
+        headRegistrarUsuario = crearHeadersParaRegistro(nombreUsuario)
+        return requests.put(Address + URIResgistro, headers=headRegistrarUsuario, data=json.dumps(bodyUsuario))
+
+    def loguearUsuario(self, nombreUsuario):
+        headLoginUsuario = crearHeadersParaElLogin(nombreUsuario)
+        return requests.get(Address + URILogin, headers=headLoginUsuario)
+
+    def pedirCandidato(self, nombreUsuario, replyLogin):
+        headPedirCandidatos = crearHeadersParaBuscarCandidatos(nombreUsuario,replyLogin.headers["Token"])
+        return requests.get(Address + URIPedirCandidato, headers=headPedirCandidatos)
+
+
+    def test_DosUsuariosConUnInteresEspecificoPidenUnCandidatoYSeEncuentranUnoAlOtro(self):
+
+        nombreUsuario1 = "test_DosUsuariosConUnInteresEspecificoPidenUnCandidatoYSeEncuentranUnoAlOtro1"
+        nombreUsuario2 = "test_DosUsuariosConUnInteresEspecificoPidenUnCandidatoYSeEncuentranUnoAlOtro2"
+
+        bodyUsuario1 = self.crearBodyConUnInteres(nombreUsuario1, self.interesCompartido)
+        bodyUsuario2 = self.crearBodyConUnInteres(nombreUsuario2, self.interesCompartido)
+
+        replyRegistro1 = self.registrarUsuario(nombreUsuario1, bodyUsuario1)
+        replyRegistro2 = self.registrarUsuario(nombreUsuario2, bodyUsuario2)
+
+        replyLogin1 = self.loguearUsuario(nombreUsuario1)
+        replyLogin2 = self.loguearUsuario(nombreUsuario2)
+
+        #Pide un candidato
+        replyPedirCandidatos1 = self.pedirCandidato(nombreUsuario1, replyLogin1)
+        replyPedirCandidatos2 = self.pedirCandidato(nombreUsuario2, replyLogin2)
+
+        self.assertEqual(replyPedirCandidatos1.reason,self.msgSeEncontraronCandidatos)
+        self.assertEqual(replyPedirCandidatos1.status_code,200)
+
+        self.assertEqual(replyPedirCandidatos2.reason,self.msgSeEncontraronCandidatos)
+        self.assertEqual(replyPedirCandidatos2.status_code,200)
+
+        self.usuariosParaBorrar.extend([nombreUsuario1, nombreUsuario2])
